8f8c3575a9b71552711a7e789835279a7197a510
rodrigo33 <rbarbosa.barbosa@gmail.com>
2023-03-20 22:21:35 -0300

    feat: Evitando modelo anêmico, enriquecendo o modelo de domínio. Definido como private os sets das propriedades. Criado classe de validação e realizado as chamadas e validações necessárias. Remoção de usings não utilizados.

--- CleanArqMvc.Domain/Entities/Category.cs
+++ CleanArqMvc.Domain/Entities/Category.cs
@@ -1,15 +1,36 @@
-﻿using System;
+﻿using CleanArqMvc.Domain.Validation;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArqMvc.Domain.Entities
 {
-    class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidateDomain(name);
+        }
+
         public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name.Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 2 characteres");
+
+            Name = name;
+        }
     }
 }
---
+++ CleanArqMvc.Domain/Validation/DomainExceptionValidation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CleanArqMvc.Domain.Validation
+{
+    public class DomainExceptionValidation : Exception
+    {
+        public DomainExceptionValidation(string error) : base(error)
+        {}
+
+        public static void When(bool hasError, string error)
+        {
+            if (hasError)
+            {
+                throw new DomainExceptionValidation(error);
+            }
+        }
+    }
+}
