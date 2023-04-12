d9da57da6fa0710d766cc0fcad56ef521b3c6bbf
rodrigo33 <rbarbosa.barbosa@gmail.com>
2023-04-12 11:40:54 -0300

    feat: Configurando entidades com o FluentApi

--- CleanArqMvc.Domain/Entities/Product.cs
+++ CleanArqMvc.Domain/Entities/Product.cs
@@ -17,7 +17,7 @@ namespace CleanArqMvc.Domain.Entities
 
         public Product(int id, string name, string description, decimal price, int stock, string image)
         {
-            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            DomainExceptionValidation.When(id < 0, "Invalid Id value.");
             Id = id;
             ValidateDomain(name, description, price, stock, image);
         }
@@ -31,29 +31,30 @@ namespace CleanArqMvc.Domain.Entities
         private void ValidateDomain(string name, string description, decimal price, int stock, string image)
         {
             DomainExceptionValidation.When(string.IsNullOrEmpty(name),
-                "Invalid name.Name is required");
+                "Invalid name. Name is required");
 
             DomainExceptionValidation.When(name.Length < 3,
-                "Invalid name, too short, minimum 3 characteres");
+                "Invalid name, too short, minimum 3 characters");
 
             DomainExceptionValidation.When(string.IsNullOrEmpty(description),
                 "Invalid description. Description is required");
 
             DomainExceptionValidation.When(description.Length < 5,
-                "Invalid description, too short, minimum 5 characteres");
+                "Invalid description, too short, minimum 5 characters");
 
             DomainExceptionValidation.When(price < 0, "Invalid price value");
 
             DomainExceptionValidation.When(stock < 0, "Invalid stock value");
 
-            DomainExceptionValidation.When(image.Length < 250,
-                "Invalid image name, too long, maximum 250 characteres");
+            DomainExceptionValidation.When(image?.Length > 250,
+                "Invalid image name, too long, maximum 250 characters");
 
             Name = name;
             Description = description;
             Price = price;
             Stock = stock;
             Image = image;
+
         }
 
         public int CategoryId { get; set; }
