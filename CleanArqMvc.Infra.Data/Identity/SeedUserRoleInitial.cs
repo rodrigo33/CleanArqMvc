f1a237a537359b1c77d014cebf02819251feecef
rodrigo33 <rbarbosa.barbosa@gmail.com>
2023-07-27 15:08:06 -0300

    feat:  Criando controlador AccountController e as viewModels na camada WebUI

---
+++ CleanArqMvc.Infra.Data/Identity/SeedUserRoleInitial.cs
@@ -0,0 +1,70 @@
+﻿using CleanArqMvc.Domain.Account;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace CleanArqMvc.Infra.Data.Identity
+{
+    public class SeedUserRoleInitial : ISeedUserRoleInitial
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public SeedUserRoleInitial(UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public void SeedUsers()
+        {
+            if (_userManager.FindByEmailAsync("usuario@localhost").Result == null)
+            {
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "usuario@localhost";
+                user.Email = "usuario@localhost";
+                user.NormalizedUserName = "USUARIO@LOCALHOST";
+                user.NormalizedEmail = "USUARIO@LOCALHOST";
+                user.EmailConfirmed = true;
+                user.LockoutEnabled = false;
+                user.SecurityStamp = Guid.NewGuid().ToString();
+
+                IdentityResult result = _userManager.CreateAsync(user, "Numsey#2021").Result;
+
+                if (result.Succeeded)
+                {
+                    _userManager.AddToRoleAsync(user, "User").Wait();
+                }
+            }
+
+            if (_userManager.FindByEmailAsync("admin@localhost").Result == null)
+            {
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "admin@localhost";
+                user.Email = "admin@localhost";
+                user.NormalizedUserName = "ADMIN@LOCALHOST";
+                user.NormalizedEmail = "ADMIN@LOCALHOST";
+                user.EmailConfirmed = true;
+                user.LockoutEnabled = false;
+                user.SecurityStamp = Guid.NewGuid().ToString();
+
+                IdentityResult result = _userManager.CreateAsync(user, "Numsey#2021").Result;
+
+                if (result.Succeeded)
+                {
+                    _userManager.AddToRoleAsync(user, "Admin").Wait();
+                }
+            }
+        }
+
+        public void SeedRoles()
+        {
+            if (!_roleManager.RoleExistsAsync("User").Result)
+            {
+                iD
+            }
+        }
+
+       
+    }
+}
--- CleanArqMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArqMvc.Infra.IoC/DependencyInjection.cs
@@ -1,9 +1,12 @@
 ﻿using CleanArqMvc.Application.Interfaces;
 using CleanArqMvc.Application.Mappings;
 using CleanArqMvc.Application.Services;
+using CleanArqMvc.Domain.Account;
 using CleanArqMvc.Domain.Interfaces;
 using CleanArqMvc.Infra.Data.Context;
+using CleanArqMvc.Infra.Data.Identity;
 using CleanArqMvc.Infra.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,11 +24,22 @@ namespace CleanArchMvc.Infra.IoC
              options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
             ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+                options.AccessDeniedPath = "/Account/Login");
+
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddScoped<IProductService, ProductService>();
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
             var myhandlers = AppDomain.CurrentDomain.Load("CleanArqMvc.Application");
